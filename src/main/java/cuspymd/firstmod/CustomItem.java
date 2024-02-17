e17dd1bec270dfdde2f5e489197083c9ad7b0cd8
Myoungdo Park <cuspymd@gmail.com>
2024-02-17 17:00:34 +0900

    Init code

---
+++ src/main/java/cuspymd/firstmod/CustomItem.java
@@ -0,0 +1,22 @@
+package cuspymd.firstmod;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class CustomItem extends Item {
+
+    public CustomItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
+        return super.use(world, user, hand);
+    }
+}
---
+++ src/main/java/cuspymd/firstmod/FirstMod.java
@@ -0,0 +1,32 @@
+package cuspymd.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("first-mod");
+	public static final Item CUSTOM_ITEM = new CustomItem(new FabricItemSettings().maxCount(5));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM, new Identifier("first-mod", "custom_item"), CUSTOM_ITEM);
+		FuelRegistry.INSTANCE.add(CUSTOM_ITEM, 300);
+	}
+}
