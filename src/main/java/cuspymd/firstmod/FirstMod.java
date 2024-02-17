f08c04a039f2d668ffeccd25640b04e1c240380c
Myoungdo Park <cuspymd@gmail.com>
2024-02-17 18:52:05 +0900

    Add text and recipe

--- src/main/java/cuspymd/firstmod/FirstMod.java
+++ src/main/java/cuspymd/firstmod/FirstMod.java
@@ -3,10 +3,13 @@ package cuspymd.firstmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,13 @@ public class FirstMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("first-mod");
 	public static final Item CUSTOM_ITEM = new CustomItem(new FabricItemSettings().maxCount(5));
+	private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(CUSTOM_ITEM))
+			.displayName(Text.translatable("itemGroup.first-mod.test_group"))
+			.entries((context, entries) -> {
+				entries.add(CUSTOM_ITEM);
+			})
+			.build();
 
 	@Override
 	public void onInitialize() {
@@ -26,7 +36,14 @@ public class FirstMod implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 
+		Registry.register(Registries.ITEM_GROUP, new Identifier("first-mod", "test_group"), ITEM_GROUP);
 		Registry.register(Registries.ITEM, new Identifier("first-mod", "custom_item"), CUSTOM_ITEM);
 		FuelRegistry.INSTANCE.add(CUSTOM_ITEM, 300);
+
+//		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+//			content.addAfter(Items.OAK_DOOR, CUSTOM_ITEM);
+//		});
+
+
 	}
 }
